b25a93b182f7db0a1e44417d69113512db33528c
Vitalii Skubii <svorder@ukr.net>
2021-02-22 21:08:11 +0200

    prettier

--- src/components/FriendList/FriendList.js
+++ src/components/FriendList/FriendList.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import FriendListItem from './FriendsListItem'
-import style from './FriendList.module.css'
+import FriendListItem from './FriendsListItem';
+import style from './FriendList.module.css';
 
 const FriendList = ({ friends }) => {
-    if(friends.length===0)return null
-    return (
-        <ul className={style.friendList}>
-            {friends.map(FriendListItem)}
-</ul>
-    )
-}
+  if (friends.length === 0) return null;
+  return <ul className={style.friendList}>{friends.map(FriendListItem)}</ul>;
+};
 
 export default FriendList;
--- src/components/Statistics/StatisticsList.js
+++ src/components/Statistics/StatisticsList.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import style from './StatisticsList.module.css'
-import PropTypes from 'prop-types'
+import style from './StatisticsList.module.css';
+import PropTypes from 'prop-types';
 const StatisticsListItem = ({ id, label, percentage }) => {
-     return (<li key={id} className={style.item}>
-         <span className={style.label}>{label}</span>
+  return (
+    <li key={id} className={style.item}>
+      <span className={style.label}>{label}</span>
       <span className={style.percentage}>{percentage}%</span>
-    </li>)
-}
-const StatisticsList = ({items}) => {
-    
-        if(items.length===0) return null
-  return (<ul className={style.statList}>{items.map(StatisticsListItem)}
-    
-  </ul>)
-}
+    </li>
+  );
+};
+const StatisticsList = ({ items }) => {
+  if (items.length === 0) return null;
+  return <ul className={style.statList}>{items.map(StatisticsListItem)}</ul>;
+};
 
 StatisticsListItem.propTypes = {
-  id:PropTypes.string.isRequired,
-  label:PropTypes.string.isRequired,
-  percentage:PropTypes.number.isRequired
-}
+  id: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  percentage: PropTypes.number.isRequired,
+};
 
 export default StatisticsList;
-
